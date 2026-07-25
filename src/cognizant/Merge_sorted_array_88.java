28b069db16ba1a1b1cd5792ddc35cfd557962af0
CodingPheonix <sybiswas115@gmail.com>
2026-07-25 21:57:18 +0530

    finished

---
+++ src/cognizant/Merge_sorted_array_88.java
@@ -0,0 +1,33 @@
+package cognizant;
+
+import java.util.Arrays;
+
+public class Merge_sorted_array_88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    static void main() {
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        int[] nums2 = new int[]{2,5,6};
+        int m = 3, n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+}
