2af41d111eb9b9344c0bdc66f3a05f532d14e540
CodingPheonix <sybiswas115@gmail.com>
2026-07-26 11:39:58 +0530

    finished

---
+++ src/cognizant/_3sum_15.java
@@ -0,0 +1,45 @@
+package cognizant;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _3sum_15 {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i+1, k = nums.length - 1;
+
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0){
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++; k--;
+
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                } else {
+                    if (sum < 0 ){
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+    static void main() {
+        int[] arr = new int[]{-1,0,1,2,-1,-4};
+        List<List<Integer>> list = threeSum(arr);
+        System.out.println(list);
+    }
+}
