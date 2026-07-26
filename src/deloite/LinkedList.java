4d68dd068ca6b1de15383e529c718106e1b58233
CodingPheonix <sybiswas115@gmail.com>
2026-07-26 15:38:49 +0530

    finished

---
+++ src/deloite/LinkedList.java
@@ -0,0 +1,26 @@
+package deloite;
+
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class LinkedList {
+    ListNode head;
+
+    public void printLinkedList() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) {
+                System.out.print(" -> "); // nice arrow formatting
+            }
+            current = current.next;
+        }
+        System.out.println(); // move to new line after printing
+    }
+}
